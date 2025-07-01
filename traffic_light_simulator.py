9bd0fc76fc7447723fc31886b3e4ffc0af5ec56f
Maruthi prasanna-25 <maruthiprasannaeemani@gmail.com>
2025-07-01 10:51:10 +0530

    comitted successfully

---
+++ traffic_light_simulator.py
@@ -0,0 +1,9 @@
+l=input("enter traffic light color:").lower()
+if l=="red":
+    print("Stop")
+elif l=="orange":
+    print("Wait")
+elif l=="green":
+    print("Go")
+else:
+    print("invalid input")
