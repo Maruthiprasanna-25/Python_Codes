1fe5901c56c10c0350b98f9ec67718c497fd324f
Maruthi prasanna-25 <maruthiprasannaeemani@gmail.com>
2025-07-05 16:06:13 +0530

    committed successfully

---
+++ generate_even_odd_nums.py
@@ -0,0 +1,16 @@
+n=int(input("enter number:"))
+if n>0: 
+    if n%2==0:
+        for i in range(2,n+1,2):
+             print(i)
+    else:
+        for i in range(1,n+1,2):
+            print(i)
+else:
+    n=-n
+    if n%2==0:
+        for i in range(2,n+1,2):
+             print(-i)
+    else:
+        for i in range(1,n+1,2):
+            print(-i)
